319da12505395010ce15468b95e9bb78d73a5763
usa-shmccoy@deloitte.com <shmccoy@deloitte.com>
2023-08-08 10:30:52 -0500

    provider block

---
+++ testmodule/main.tf
@@ -0,0 +1,14 @@
+terraform {
+ cloud {
+ organization = "Pirates of the Carribean"
+ workspaces {
+ name = "The-High-Seas-Workspace"
+ }
+ }
+ required_providers {
+ aws = {
+ source = "hashicorp/aws"
+ version = "~> 4.4.0"
+ }
+}
+required_version = ">= 1.1.0"
